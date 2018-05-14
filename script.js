e0ba64966944e31a330c9dfd5a07ccf6c7d358b2
Kamalesh Palanisamy <kamalesh800@gmail.com>
2018-05-14 13:44:14 +0400

    Completed the typing speed checker

---
+++ script.js
@@ -0,0 +1,25 @@
+var text = document.getElementById("text");
+var character = 0;
+var check = document.getElementById("test").textContent;
+var text1=''
+var start = 0;
+var stop=0;
+var speed = 0;
+text.addEventListener('keydown',function(){
+	if(character==0){
+		start = new Date().getTime();
+	}
+	text1 = text.value
+	if (check[character]!==text1[character]){
+		text.maxLength=character+1;
+	}
+	else{
+		character++;
+		text.maxLength = check.length;
+	}
+	if(character == check.length){
+		stop = new Date().getTime();
+		speed = (check.length)/((stop-start)/1000);
+		alert("Your typing speed is "+speed+" words/second")
+	}
+})
